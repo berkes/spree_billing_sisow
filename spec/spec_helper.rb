b27785bdf72c5d04b8105c9df8aad1897d2865a0
Bèr Kessels <ber@berk.es>
2015-04-03 13:57:47 +0200

    Include and use rspec-activemodel-mocks to continue using mock_model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
 require 'webmock/rspec'
+require 'rspec/active_model/mocks'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
