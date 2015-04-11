094a9ee48918f52d8a3e723f90e059fe05875ad7
Bèr Kessels <ber@berk.es>
2015-04-11 16:09:53 +0200

    Clean up decorator.

---
+++ app/overrides/admin_decorator.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
+                     :name => "add_sisow_settings",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                     :partial => "spree/admin/shared/configurations_menu_sisow")
