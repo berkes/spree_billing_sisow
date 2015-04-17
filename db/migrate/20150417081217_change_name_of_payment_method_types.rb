7e4d2d6802cac9ff688c96886f22584fec4e8bd3
Bèr Kessels <ber@berk.es>
2015-04-17 10:28:44 +0200

    Add migration to change the names of already made payment methods.

---
+++ db/migrate/20150417081217_change_name_of_payment_method_types.rb
@@ -0,0 +1,22 @@
+class ChangeNameOfPaymentMethodTypes < ActiveRecord::Migration
+  def up
+    name_maps.each do |old, new|
+      Spree::PaymentMethod.where(type: old).update_all(type: new)
+    end
+  end
+
+  def down
+    name_maps.each do |old, new|
+      Spree::PaymentMethod.where(type: new).update_all(type: old)
+    end
+  end
+
+  private
+  def name_maps
+    {
+     'Spree::BillingIntegration::SisowBilling::Ideal' => 'Spree::PaymentMethod::SisowBilling::Ideal',
+     'Spree::BillingIntegration::SisowBilling::Bancontact' => 'Spree::PaymentMethod::SisowBilling::Bancontact',
+     'Spree::BillingIntegration::SisowBilling::Sofort' => 'Spree::PaymentMethod::SisowBilling::Sofort'
+    }
+  end
+end
