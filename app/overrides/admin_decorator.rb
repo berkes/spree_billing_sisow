3170682583f16dddd17e46289c13ee37ddd3561a
Bèr Kessels <ber@berk.es>
2019-07-29 13:58:04 +0200

    Fix deprecation warning by specifying the hash of the parent

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+  original: "0faac568db7e03f5705d3f86553d21a58bda03aa",
   virtual_path: "spree/admin/shared/sub_menu/_configuration",
   name: "add_sisow_settings",
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
