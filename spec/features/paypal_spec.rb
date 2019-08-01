0fba88fc63c8091d98c350325b2f582706aa46f3
Bèr Kessels <ber@berk.es>
2019-08-01 14:54:35 +0200

    Move Paypal specs into own specfile

--- spec/features/paypal_spec.rb
+++ spec/features/paypal_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
-feature "checkout" do
+feature "paypal checkout" do
   let(:user) { create(:user) }
   let(:order) { OrderWalkthrough.up_to(:payment) }
   let(:sisow_request_url) do
@@ -15,36 +17,28 @@ feature "checkout" do
       to_return(redirect_url_response)
   end
 
-  context "site has has paymentmethod iDeal" do
-    let(:ideal) { Spree::PaymentMethod::SisowBilling::Ideal.create!(name: "iDeal") }
-    let(:redirect_url_response) { stored_response("ideal_redirect_url_output") }
+  context "site has has paymentmethod Paypal" do
+    let(:paypal) do
+      Spree::PaymentMethod::SisowBilling::Paypalec.create!(name: "Paypal")
+    end
+    let(:redirect_url_response) do
+      stored_response("paypal_redirect_url_output")
+    end
     let(:sisow_request_params) do
       { amount: "2999",
         callbackurl: "http://www.example.com/sisow/#{order.number}",
         cancelurl: "http://www.example.com/orders/#{order.number}/checkout/sisow_cancel",
         description: "Spree Test Store - Order: #{order.number}",
-        issuerid: "09", # Triodos Bank
-        merchantid: '2537407799',
+        merchantid: "2537407799",
         notifyurl: "http://www.example.com/sisow/#{order.number}",
-        payment: "ideal",
+        payment: "paypalec",
         purchaseid: order.number,
         returnurl: "http://www.example.com/orders/#{order.number}/checkout/sisow_return",
-        shop_id: "" }
-      # Dynamic params, omitted
-      # entrancecode: 'PIL6IDY7',
-      # sha1: 'e265b37b3256de19661793cae2abc0866c0207d5',
+        shop_id: '' }
     end
-    let(:sisow_directory_url) do
-      "http://www.sisow.nl/Sisow/iDeal/RestHandler.ashx/DirectoryRequest"
-    end
-    let(:sisow_directory_params) { { merchantid: "2537407799" } }
 
     before do
-      allow(order).to receive_messages(available_payment_methods: [ideal])
-
-      stub_request(:get, sisow_directory_url).
-        with(query: hash_including(sisow_directory_params)).
-        to_return(stored_response("ideal_issuer_output"))
+      allow(order).to receive_messages(available_payment_methods: [paypal])
 
       visit spree.checkout_state_path(:payment)
 
@@ -54,14 +48,9 @@ feature "checkout" do
       Capybara.page.driver.options[:follow_redirects] = false
     end
 
-    after do
-      Capybara.page.driver.options[:follow_redirects] = true
-    end
-
-    scenario "I select 'Triodos Bank' it creates a transaction at Sisow and redirects me there" do
-      select "Triodos Bank", from: :issuer_id
+    scenario "I choose the only payment option Paypal, and Sisow and redirects me there" do
       click_button "Save and Continue"
-      expect(WebMock).to have_requested(:get, sisow_request_url). with(query: hash_including(sisow_request_params))
+      expect(WebMock).to have_requested(:get, sisow_request_url).with(query: hash_including(sisow_request_params))
       response = page.driver.response
       expect(response.status).to be 302
       expect(response.headers["Location"]).to match(%r(https://www\.sisow\.nl/Sisow/iDeal/Simulator\.aspx\?merchantid=2537407799&txid=\w*&sha1=\w*))
